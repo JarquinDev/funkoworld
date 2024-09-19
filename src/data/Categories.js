967d126eb573dad0589197c13c084380b8ea4e86
cjarking <68793002+cjarking@users.noreply.github.com>
2024-09-19 17:16:08 -0600

    update de todas las rutas

--- src/data/Categories.js
+++ src/data/Categories.js
@@ -1,11 +1,11 @@
 const categories = [
-    { id: 1, name: 'Sci-Fi', image: './img/darth-maul1.png',namepath:'Sci-Fi' },
-    { id: 2, name: 'Video Games', image: './img/malthael1.png',namepath:'Video Games' },
-    { id: 3, name: 'Music', image: './img/snoop1.png',namepath:'Music' },
-    { id: 4, name: 'Anime', image: './img/edward1.png',namepath:'Anime' },
-    { id: 5, name: 'Comics', image: './img/comic1.png',namepath:'Comics' },
-    { id: 6, name: 'Cartoons', image: './img/marvin1.png',namepath:'Cartoons' },
-    { id: 7, name: 'TV And Series', image: './img/eddie.png',namepath:'TV-Series' },
-    { id: 8, name: 'Sports', image: './img/lebron1.png',namepath:'Sports' },
+    { id: 1, name: 'Sci-Fi', image: `${process.env.PUBLIC_URL}/img/darth-maul1.png`,namepath:'Sci-Fi' },
+    { id: 2, name: 'Video Games', image: `${process.env.PUBLIC_URL}/img/malthael1.png`,namepath:'Video Games' },
+    { id: 3, name: 'Music', image: `${process.env.PUBLIC_URL}/img/snoop1.png`,namepath:'Music' },
+    { id: 4, name: 'Anime', image: `${process.env.PUBLIC_URL}/img/edward1.png`,namepath:'Anime' },
+    { id: 5, name: 'Comics', image: `${process.env.PUBLIC_URL}/img/comic1.png`,namepath:'Comics' },
+    { id: 6, name: 'Cartoons', image: `${process.env.PUBLIC_URL}/img/marvin1.png`,namepath:'Cartoons' },
+    { id: 7, name: 'TV And Series', image: `${process.env.PUBLIC_URL}/img/eddie.png`,namepath:'TV-Series' },
+    { id: 8, name: 'Sports', image: `${process.env.PUBLIC_URL}/img/lebron1.png`,namepath:'Sports' },
 ]
 export default categories;
